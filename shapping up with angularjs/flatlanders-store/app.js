ef6b9bf844a5259ff5b2b99debd46b23e6117402
Cristian R. Silva <rs.cristian@gmail.com>
2016-06-15 00:20:03 -0300

    Added new Custom Directives for tabs, reviews and gallery

--- shapping up with angularjs/flatlanders-store/app.js
+++ shapping up with angularjs/flatlanders-store/app.js
@@ -6,27 +6,6 @@
     this.products = gems;               
   });
 
-  app.controller('TabController', function(){
-    this.tab = 1;
-    
-    this.setTab = function(newTab){
-      this.tab = newTab;
-    }
-    
-    this.isSet = function(tab){
-      return this.tab === tab;
-    }
-
-  });
-
-  app.controller('GalleryController', function(){
-    this.current = 0;
-
-    this.setCurrent = function(newGallery){
-      this.current = newGallery || 0;
-    };
-  });
-
   app.controller('ReviewController', function(){
     this.review = {};
 
@@ -38,31 +17,65 @@
 
   });
 
-  app.directive('productDescription', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-description.html'
-    };
-  });  
-
-  app.directive('productSpecs', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'product-specs.html'
-    };
-  }); 
-
   app.directive('productTabs', function(){
     return {
       restrict: 'E',
       templateUrl: 'product-tabs.html',
       controller:function(){
         
+        this.tab = 1;
+
+        this.isSet = function(checkTab) {
+          return this.tab === checkTab;
+        };
+
+        this.setTab = function(setTab) {
+          this.tab = setTab;
+        };
+
       },
       controllerAs: 'tab'
     };
+  });  
+
+  app.directive("productDescription", function(){
+    return {
+      restrict: 'E',
+      templateUrl: "product-description.html"
+    };
+  });
+
+  app.directive("productReviews", function(){
+    return {
+      restrict: 'E',
+      templateUrl: "product-reviews.html"
+    };
+  });
+
+  app.directive("productSpecs", function(){
+    return {
+      restrict:"A",
+      templateUrl: "product-specs.html"
+    };
   });
      
+
+  app.directive('productGallery', function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'product-gallery.html',
+      controller:function(){
+        
+        this.current = 0;
+
+        this.setCurrent = function(newGallery){
+          this.current = newGallery || 0;
+        };
+                
+      },
+      controllerAs: 'gallery'
+    };
+  });      
 
   var gems = [
     {
